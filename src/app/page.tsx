9c4398b8456ccb70c0a87e1944da19237dddbfb2
frdrk00 <masterkooo@gmail.com>
2023-06-17 21:01:56 +0200

    Subreddit detail page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+          <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
             <div className="flex justify-between gap-x-4 py-3">
               <p className="text-zinc-500">
                 Your personal Breadit homepage. Come here to check in with your
@@ -34,7 +34,7 @@ export default function Home() {
             >
               Create Community
             </Link>
-          </div>
+          </dl>
         </div>
       </div>
     </>
