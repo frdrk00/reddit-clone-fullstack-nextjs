335ea797b6986d2c3f397b2d2581352ce787005e
frdrk00 <masterkooo@gmail.com>
2023-06-18 15:09:53 +0200

    Performance-optimized post detail page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import { buttonVariants } from '@/components/ui/Button';
-import { HomeIcon } from 'lucide-react';
-import Link from 'next/link';
+import CustomFeed from '@/components/CustomFeed'
+import GeneralFeed from '@/components/GeneralFeed'
+import { buttonVariants } from '@/components/ui/Button'
+import { getAuthSession } from '@/lib/auth'
+import { HomeIcon } from 'lucide-react'
+import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession()
+
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
       <div className="grid gird-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-        {/* feed */}
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
 
-        {/* seubreddit info */}
+        {/* subreddit info */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
           <div className="bg-emerald-100 px-6 py-4">
             <p className="font-semibold py-3 flex items-center gap-1.5">
@@ -38,5 +44,5 @@ export default function Home() {
         </div>
       </div>
     </>
-  );
+  )
 }
--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,16 +1,18 @@
-import dynamic from 'next/dynamic';
-import Image from 'next/image';
-import { FC } from 'react';
+'use client'
+
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
+import { FC } from 'react'
 
 const Output = dynamic(
   async () => (await import('editorjs-react-renderer')).default,
   {
     ssr: false,
   }
-);
+)
 
 interface EditorOutputProps {
-  content: any;
+  content: any
 }
 
 const style = {
@@ -18,12 +20,12 @@ const style = {
     fontSize: '0.875rem',
     lineHeight: '1.25rem',
   },
-};
+}
 
 const renderers = {
   image: CustomImageRenderer,
   code: CustomCodeRenderer,
-};
+}
 
 const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
   return (
@@ -34,26 +36,26 @@ const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
       className="text-sm"
       renderers={renderers}
     />
-  );
-};
+  )
+}
 
 function CustomCodeRenderer({ data }: any) {
-  data;
+  data
   return (
     <pre className="bg-gray-800 rounded-md p-4">
       <code className="text-gray-100 text-sm">{data.code}</code>
     </pre>
-  );
+  )
 }
 
 function CustomImageRenderer({ data }: any) {
-  const src = data.file.url;
+  const src = data.file.url
 
   return (
     <div className="relative w-full min-h-[15rem]">
       <Image alt="image" className="object-contain" fill src={src} />
     </div>
-  );
+  )
 }
 
-export default EditorOutput;
+export default EditorOutput
